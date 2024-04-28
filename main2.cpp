55aee9581647abecba8fd9591fcbf4c9f857400a
Dakshchaturvedi <106811707+Dakshchaturvedi@users.noreply.github.com>
2024-04-28 10:50:20 +0530

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-  int list[] = {1, 2, 3, 4, 5};
+  int list[] = {4, 6, 19, 12, 12};
   int size = sizeof(list) / sizeof(list[0]);
   if (size == 0) {
     cout << "The list is empty." << endl;
     return 0;
   }
   if (size == 1) {
-    cout << "The list has only one element." << endl;
+    cout << "in this only one element:" << endl;
     return 0;
   }
-  cout << "The second element in the list is " << list[1] << endl;
+  cout << "the second integer in List is: " << list[1] << endl;
 
   return 0;
 }
